6daa223da6068bcecac898c2ec6712996b8fdbc0
ratikasubarzaa <ratika.subarza@finaccel.co>
2024-01-31 15:06:50 +0700

    First Commit Java Hello World

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,25 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+//        Sama kayak print
+        System.out.printf("Hello and welcome!");
+
+//        Print new line
+        System.out.println("My name Ratika");
+        System.out.print("I am QA Engineer");
+
+        //    variable
+        boolean flag; //Try - catch
+        float num;
+
+//        Looping 1 to 5
+        for (int i = 1; i <=5; i++){
+
+            System.out.println("i = "+1);
+
+        }
+
+    }
+
+}
